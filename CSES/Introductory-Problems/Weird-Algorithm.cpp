e5b4e774a30e3bdee675f3694c0d0e671810b115
Huỳnh Phúc Nguyên <simonsimonphucnguyen2304@gmail.com>
2026-02-24 20:15:30 +0700

    Create Weird-Algorithm.cpp

---
+++ CSES/Introductory-Problems/Weird-Algorithm.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    long long n ; 
+    cin >> n; 
+    cout << n << " "; 
+    while (n > 1){
+        if (n % 2 == 0){
+            n /= 2; 
+        }
+        else{
+            n = 3*n + 1; 
+        }
+        cout << n << " " ; 
+    }
+    return 0;
+}
