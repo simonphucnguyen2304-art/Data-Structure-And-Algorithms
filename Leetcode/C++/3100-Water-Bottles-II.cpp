93bde99daeae41d29e263007d7149e7ee135bf47
Huỳnh Phúc Nguyên <simonsimonphucnguyen2304@gmail.com>
2026-02-28 14:39:21 +0700

    Create 3100-Water-Bottles-II.cpp

---
+++ Leetcode/C++/3100-Water-Bottles-II.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int total = numBottles; 
+        while (numBottles >= numExchange){
+            numBottles -= numExchange; 
+            ++numExchange;
+            ++total;
+            ++numBottles;  
+        }
+        return total; 
+    }
+};
